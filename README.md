bdede7861ac0b4389ef9bfe62558a741b248f3e4
Ron Forrester <rjf@m1.home>
2023-07-17 21:58:12 -0700

    Comment, add release build optimization, general cleanup

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 
+## Rust on ESP32-c3/6 with STD, Simplified
+
+There are lots of great references for people wanting to use Rust on the extensa ESP32-c? series of micro-controllers, but even so, I've struggled for a few weeks to find any one reference that just worked out of the box (based on the instructions). Being newish to Rust, and even newer to Rust in embedded programming (I have solid Arduino knowledge) didn't enhance my success.
+
+However, recently I believe I've stumbled on the most straightforward set of steps necessary to do ESP-c? development in Rust, on a modern MacOS machine. This project attempts to lay those steps out in the hope that someone else finds it useful. Stay tuned to the end of these instructions for ideas on how it could be even better (and PR's helping will be much appreciated).
+
+### Prerequisites
+
+These instructions assume the following:
+
+* You're using a relatively recent version of Rust and have the nightly toolchain installed. At the time of this writing I am using v1.73.0-nightly.
+* You're on relatively modern version MacOS. I am on 13.4 (Ventura). These instructions may work on other operating systems, e.g. Linux, but will almost certainly need some adjustments. See requests for PR's to make this better.
+* Clone this repos!
+* A working ESP32-C3 or ESP32-C6 micro-controller, a breadboard (and jumpers), an LED, and a resistor around 1K
+
+### Install dependencies
+
+You'll need `espflash` installed:
+
+```
+cargo install espflash
+```
+
+You'll need `espup` installed:
+
+```
+cargo install espup
+```
+
+Once that finishes installing, use it to install the extensa Rust toolchain/support:
+
+```
+cd ~
+espup install
+```
+
+In addition to installing all the necessary Rust support, it will create the file `~/export-esp.sh` for you. Source this ( `. ~/export-esp.sh` ) to get the environment all setup, or add it to your shell's startup if you're going to be doing this a lot. I've tested this on both `bash` and `zsh`.
+
+Now you're ready to try and build one of the samples in this repos. But before doing so, edit `main.rs` and change the `WIFI_SSID` and `WIFI_PW` constants to match your network, then save the file and do the following:
+
+```
+cd esp32-c3-test
+cargo update
+cargo build
+```
+
+If you don't get a successful build out of this step, trace your steps and make sure you have all the dependencies above accounted for.
+
+Now, If you have, in this case, a working ESP32-C3 (e.g., [this one from Adafruit](https://www.adafruit.com/product/5337) ), wired up in a breadboard configured as pictureed, you're ready to attempt to load your code onto the micro-controller and see it work:
+
+```
+cargo run
+```
+
+At which point you'll see `espflash` get run and it will ask you for the [USB] serial port the device is on. Select the appropriate port (for me, the first one in the list is always right), and hit enter. Once the code is uploaded and starts running, the sample does the following:
+
+1. Connects to the WiFi network you configured above
+2. Connects to https://slashdot.org and does a GET request for the home page, outputting the first 3K of it to the console
+3. Drops into an infinite loop, blinking an LED on GPIO 1 (on the C3), or GPIO 4 (on the C6).
+
+Do note that if you have the ESP32-C6 and are building that sample, the breadboard configuration is slightly different.
+
+## Requests for help
+
+As I said, I am using this project to continue my Rust journey, which I am early into. I have a garden project running on an ESP8266 using Arduino (C) code, and would like to convert it over to Rust on the ESP32-C3. 
+
+All that is to say, I have a very basic understanding, and would love more expert help on this sample project in the following areas:
+
+1. The code for the C3 and C6 samples are identical except for the GPIO pin used for the LED. I would love to collapse the sample into a single code base, since other than the LED the only differences are build flag for the two different processors. I am not sure how to accommodate this myself.
+2. Test these instructions on Linux (or Windows) and help me update with any differences so more people on more platforms can use this for ESP32 development.
+3. Expand the samples to do other things. Both the C3 and C6 have an addressable RGB LED that I am not sure how to control. Would be cool to expand the samples to demo this LED.
+
--- esp32-c3-test/src/main.rs
+++ esp32-c3-test/src/main.rs
@@ -5,30 +5,47 @@ use std::thread;
 use std::time::Duration;
 
 use embedded_svc::wifi::{ClientConfiguration, Configuration};
+use esp_idf_hal::gpio::Gpio1;
 use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::gpio::Pins;
+use esp_idf_hal::modem::Modem;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
 
-fn test_https_client() -> anyhow::Result<(), anyhow::Error> {
+// TODO: Set these appropriate for your desired wifi network
+//
+static WIFI_SSID: &str = "CountrySide";
+static WIFI_PW: &str = "%Burn1n4t1ng%";
+
+// Using our WiFi connection, make a [GET] request to the URL below and output the first 3K
+// of the response.
+//
+fn make_https_request() {
     use embedded_svc::http::client::*;
     use embedded_svc::utils::io;
     use esp_idf_svc::http::client::*;
 
-    let url = String::from("https://slashdot.org");
+    static URL: &str = "https://slashdot.org";
+    info!("About to fetch content from {}", URL);
 
-    info!("About to fetch content from {}", url);
+    let mut client = Client::wrap(
+        EspHttpConnection::new(&Configuration {
+            crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
+            ..Default::default()
+        })
+        .expect("Failed to create http client"),
+    );
 
-    let mut client = Client::wrap(EspHttpConnection::new(&Configuration {
-        crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
-
-        ..Default::default()
-    })?);
-
-    let mut response = client.get(&url)?.submit()?;
+    let mut response = client
+        .get(URL)
+        .expect("Failed to get valid URL")
+        .submit()
+        .expect("Failed to get http response");
 
     let mut body = [0_u8; 3048];
-
-    let read = io::try_read_full(&mut response, &mut body).map_err(|err| err.0)?;
+    let read = io::try_read_full(&mut response, &mut body)
+        .map_err(|err| err.0)
+        .expect("Failed to read full response from server");
 
     info!(
         "Body (truncated to 3K):\n{:?}",
@@ -36,62 +53,97 @@ fn test_https_client() -> anyhow::Result<(), anyhow::Error> {
     );
 
     // Complete the response
-    while response.read(&mut body)? > 0 {}
-
-    Ok(())
+    while response
+        .read(&mut body)
+        .expect("Failed to read remaining response")
+        > 0
+    {}
 }
-fn main() -> Result<(), anyhow::Error> {
-    esp_idf_sys::link_patches();
-    esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
-    let sys_loop = EspSystemEventLoop::take().unwrap();
-    let nvs = EspDefaultNvsPartition::take().unwrap();
+// Connect to a WiFi network who's credentials are in WIFI_SSID and WIFI_PW. Note
+// this method retries indefinitely until it connects, so check the console output
+// if you're not seeing the connect made
+//
+fn connect_to_wifi(modem: Modem) -> EspWifi<'static> {
+    //
+    let sys_loop = EspSystemEventLoop::take().expect("Failed to get event loop");
+    let nvs = EspDefaultNvsPartition::take().expect("Failed to get nvs partition");
 
-    let mut wifi_driver = EspWifi::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
+    let mut wifi_driver =
+        EspWifi::new(modem, sys_loop, Some(nvs)).expect("Failed to get wifi_driver");
 
     wifi_driver
         .set_configuration(&Configuration::Client(ClientConfiguration {
-            ssid: "CountrySide".into(),
-            password: "%Burn1n4t1ng%".into(),
+            ssid: WIFI_SSID.into(),
+            password: WIFI_PW.into(),
             ..Default::default()
         }))
-        .unwrap();
+        .expect("Failed to configure the WiFi driver");
 
-    wifi_driver.start().unwrap();
-    wifi_driver.connect().unwrap();
-    while !wifi_driver.is_connected().unwrap() {
-        let config = wifi_driver.get_configuration().unwrap();
-        println!("Waiting for station {:?}", config);
+    wifi_driver.start().expect("Failed to start WiFi driver");
+    wifi_driver
+        .connect()
+        .expect("Failed to connect operation on WiFi driver");
+
+    while !wifi_driver
+        .is_connected()
+        .expect("Something failed during connection attempt")
+    {
+        let config = wifi_driver
+            .get_configuration()
+            .expect("Failed to get the configuration for the wifi driver");
+
+        info!("Waiting for station {:?}", config);
         thread::sleep(Duration::from_millis(1000));
     }
 
-    println!("Should be connected now");
-    // loop {
-    println!(
+    info!("Should be connected now");
+    info!(
         "IP info: {:?}",
-        wifi_driver.sta_netif().get_ip_info().unwrap()
+        wifi_driver
+            .sta_netif()
+            .get_ip_info()
+            .expect("Failed to get the IP address for our connection")
     );
-    thread::sleep(Duration::new(10, 0));
-    // }
 
-    let _ = test_https_client();
+    // Just pause for effect, i.e. to read the console output for errors, etc.
+    //
+    thread::sleep(Duration::from_secs(10));
 
-    // let peripherals = Peripherals::take().unwrap();
-    let pin = peripherals.pins.gpio1;
+    wifi_driver // return with static lifetime so WiFi connection stays active even after return
+}
 
-    // esp-idf-hal >= "0.39.3"
-    let mut led = PinDriver::output(pin)?;
+fn main() {
+    //
+    esp_idf_sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
 
+    // TODO: Peripherals is an odd type of singleton -- as far as I can tell, you can only get it once. Is there a way to get it more than once?
+    let peripherals = Peripherals::take().expect("failed to get the available peripherals");
+
+    // Attempt to connect to WiFi -- will not return until it
+    // successfully connects -- we capture the return value so
+    // the compiler doesn't drop it and sever the wifi connection
+    //
+    let _wifi: EspWifi<'static> = connect_to_wifi(peripherals.modem);
+
+    // Make a basic https request to ensure WiFi connection is working
+    //
+    make_https_request();
+
+    // This method never returns, loops forever blinking an LED on GPIO 4 (for esp32-C6) or GPIO1 (for esp32-c3)
+    //
+    let pins = peripherals.pins;
+    blink_led(pins);
+}
+
+// Loop forever blinking the appropriate LED
+//
+fn blink_led(pins: Pins) {
+    let pin: Gpio1 = pins.gpio1;
+    let mut led = PinDriver::output(pin).expect("failed to get LED pin reference");
     loop {
-        led.toggle()?;
-
-        // we are using thread::sleep here to make sure the watchdog isn't triggered
-        thread::sleep(Duration::from_millis(1100));
-
-        led.set_low()?;
+        led.toggle().expect("Failed to toggle LED state");
         thread::sleep(Duration::from_millis(500));
-
-        // info!("Hello, world!");
     }
 }
